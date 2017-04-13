0cfdb17d9624a7ca8f2641e8e2afbdf5d65e6cae
Tim <t1mb0b@users.noreply.github.com>
2017-04-13 12:06:24 -0600

    Create GlideRecord2Array.js

---
+++ GlideRecord2Array.js
@@ -0,0 +1,10 @@
+function activeValue() {
+	var appAray = [];
+	var u_app_mod = new GlideRecord('u_app_mod');  
+    u_app_mod.addQuery('u_active', 'true');  
+    u_app_mod.query();   
+    while (u_app_mod.next()) {   
+  		appAray.push(u_app_mod.getValue('u_application'));  
+    }  
+    return  appAray.join(',');  
+}
