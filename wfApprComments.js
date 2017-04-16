35eb6367bd2c557d1227a1dd2093ace9ff40f661
Tim <t1mb0b@users.noreply.github.com>
2017-04-15 20:49:38 -0600

    Update wfApprComments.js

--- wfApprComments.js
+++ wfApprComments.js
@@ -12,3 +12,33 @@ while (ac.next()) {
 	ta.update();
 	workflow.scratchpad.apprComments = apprComm;
 }
+
+====================================
+	
+task.short_description = "Process " + current.cat_item.name + " for " + current.request.requested_for.name;
+/*
+
+Paste the worknotes and approval comments in the task if they exist
+
+*/
+if (JSUtil.notNil(workflow.scratchpad.wkNotes)) {
+	task.work_notes = "\n---\nComments from Review Task: " + workflow.scratchpad.wkNotes;
+}
+if (JSUtil.notNil(workflow.scratchpad.calcWorkNotes)) {
+	task.work_notes = "\n----\nComments from Rate Calculation Task: " + workflow.scratchpad.calcWorkNotes;
+}
+if (JSUtil.notNil(workflow.scratchpad.apprComments)) {
+	task.work_notes = "\n---\nComments from Rate Calculation Approval: " + workflow.scratchpad.apprComments;
+}
+/*
+
+Get Approval Comments from Approved Rate Change
+
+*/
+var apv = new GlideRecord('sysapproval_approver');
+apv.addQuery('sysapproval',current.sys_id);
+apv.addQuery('comments','!=','');
+apv.query();
+while (apv.next()) { 
+task.work_notes = "\n---\nApproval Comments: from " + apv.approver.name + "\n" + apv.comments.getJournalEntry(1); 
+}
