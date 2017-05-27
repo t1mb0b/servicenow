674c4b5de078dbc281a3f2dbba805496d4c7f284
Tim <t1mb0b@users.noreply.github.com>
2017-05-26 18:00:55 -0600

    Update addToWatchlist.js

--- addToWatchlist.js
+++ addToWatchlist.js
@@ -1,18 +1,31 @@
-var currentWatchList = current.getValue('watch_list');
+	var currentWatchList = current.getValue('watch_list');
+	if (!currentWatchList)
+		currentWatchList = '';
+
+	gs.info("The watchlist is {0}", currentWatchList);
+	
     var u_classroom_assignments = new GlideRecord('u_classroom_assignments');
     u_classroom_assignments.addQuery('u_classroom', current.getValue('location')); //probably 'u_classroom'
     u_classroom_assignments.query();
     
+    gs.info("Found {0} classroom assignments", u_classroom_assignments.getRowCount());
+    
     while(u_classroom_assignments.next()) {
-    	if (currentWatchList.indexOf(u_classroom_assignments.getValue("u_professor")) {
-    		gs.addInfoMessage(u_classroom_assignments.getValue("u_professor") + "is already on the watchlist");
+    	var professor = u_classroom_assignments.getValue('u_professor');
+    	gs.info("The professor is {0}", professor);
+    	
+    	if (currentWatchList.indexOf(professor) > -1) {
+    		gs.info("{0} is already on the watchlist", professor);
+    		gs.addInfoMessage(professor + " is already on the watchlist");
     		continue;
     	}
     		
-    	if (!currentWatchList)
-    		currentWatchList = u_classroom_assignments.getValue('u_professor');
-    	else
-    		currentWatchList += ',' + u_classroom_assignments.getValue('u_professor');
+    	if (!currentWatchList) {
+    		gs.info("Adding {0} as the first watchlist entry", professor);
+    		currentWatchList = professor;
+    	} else {
+    		gs.info("Adding {0} to the watchlist", professor);
+    		currentWatchList += ',' + professor;
     }
-    
+    gs.info("Saving the current watchlist as {0}", currentWatchList);
     current.setValue("watch_list", currentWatchList);
