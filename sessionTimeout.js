e46313cee6f89e4b080ce589ea4fa93f5f04f10e
Tim <t1mb0b@users.noreply.github.com>
2017-06-01 13:20:05 -0600

    Update sessionTimeout.js

--- sessionTimeout.js
+++ sessionTimeout.js
@@ -1,6 +1,13 @@
+/* GLobal UI Script */
+
 function onLoad() {
     document.observe("dom:loaded", function() {
         var timeout = g_scratchpad.timeout;
         setTimeout(function() {alert('Your ServiceNow session will be logged out in 5 minutes');},60000*(timeout-5));
     });
 }
+
+/* Display BR */
+
+g_scratchpad.timeout=gs.getProperty('glide.ui.session_timeout');
+gs.log("timeout: " + gs.getProperty('glide.ui.session_timeout'));
