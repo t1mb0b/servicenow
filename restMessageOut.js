6ad67cfcb895d6bc9c772e92c731876bca28e605
Tim <t1mb0b@users.noreply.github.com>
2017-06-06 13:55:28 -0600

    Create restMessageOut.js

---
+++ restMessageOut.js
@@ -0,0 +1,13 @@
+var r = new sn_ws.RESTMessageV2('HRWF', 'get');
+
+r.setAuthentication('basic', 'oData Service Account');
+
+var response = r.execute();
+var responseBody = JSON.parse(response.getBody());
+var httpStatus = response.getStatusCode();
+var oResponse = responseBody.d;
+for (var key in oResponse) {
+  if (oResponse.hasOwnProperty(key)) {
+    gs.print(key + " -> " + oResponse[key]);
+  }
+}
