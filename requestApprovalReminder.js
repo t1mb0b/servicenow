dfcaf0d217489cb9d5dbcd880d12884a87a4355f
Tim <t1mb0b@users.noreply.github.com>
2017-08-01 21:38:31 -0600

    Update requestApprovalReminder.js

--- requestApprovalReminder.js
+++ requestApprovalReminder.js
@@ -163,3 +163,145 @@ while (app.next()) {
 		}
 	}
 })();
+
+
+=====================================================
+	
+FAIApprovalReminders.prototype = {
+	initialize: function() {
+		this.oSchedule = gs.getProperty("fai.approval_reminder.schedule");
+		this.oSchedId = gs.getProperty("fai.approval_reminder.scheduleId");
+		this.iBusinessHours = parseInt(gs.getProperty("fai.approval_reminder.interval"));
+		this.iNumReminders = parseInt(gs.getProperty("fai.approval_reminder.number"));
+		this.bCancel = gs.getProperty("fai.approval_reminder.cancel");
+		this.sEvent = "fai.approval.reminder";
+	},
+	
+	insertReminder : function (oReminderInsert) {
+		var sAppSysId = oReminderInsert.sa_sys_id.toString();
+		var dAppStart = new GlideDateTime(oReminderInsert.sa_sys_created_on);
+		var oReminder = new GlideRecord('u_approval_reminders');
+		oReminder.initialize();
+		oReminder.setValue('u_approval_for', sAppSysId);
+		oReminder.setValue('u_reminders_sent', '0');
+		oReminder.setValue('u_active', 'true');
+		oReminder.setValue('u_send_next_reminder', this._calcNextReminder(dAppStart));
+		gs.print("Inserting for " + oReminderInsert.sa_sysapproval.number + " | " + dAppStart + " | " + this._calcNextReminder(dAppStart));
+		oReminder.insert();
+	},
+	
+	sendReminder : function (sReminder,sApproval) {
+		var oReminder = new GlideRecord('u_approval_reminders');
+		if (oReminder.get(sReminder)) {
+			var iReminder = parseInt(oReminder.getValue('u_reminders_sent')) + 1;
+			var dReminder = new GlideDateTime(oReminder.u_send_next_reminder);
+			oReminder.setValue('u_reminders_sent', iReminder.toString());
+			// If we're at the maximum number of reminders, we need to the cancel date
+			if (iReminder == this.iNumReminders) {
+				oReminder.setValue('u_send_next_reminder', '');
+				oReminder.setValue('u_cancel_approval', this._calcNextReminder(dReminder));
+			} else {
+				oReminder.setValue('u_send_next_reminder', this._calcNextReminder(dReminder));
+			}
+			gs.print("Sending Reminder" + iReminder + " for " + oReminder.u_approval_for.sysapproval.number + " | " + this._calcNextReminder());
+			oReminder.update();
+			// We need to approval object to pass to the event
+			var oApproval = new GlideRecord("sysapproval_approver");
+			oApproval.get(sApproval);
+			// Trigger the event
+			gs.eventQueue(this.sEvent, oApproval, oApproval.approver, oReminder.u_reminders_sent);
+		}
+	},
+	
+	deactivateReminder : function (sReminder) {
+		gs.print("Deactivating SysId " + sReminder);
+		var oReminder = new GlideRecord('u_approval_reminders');
+		if (oReminder.get(sReminder)) {
+			// Clear all of the values on the record
+			oReminder.setValue('u_send_next_reminder', '');
+			oReminder.setValue('u_cancel_approval', '');
+			oReminder.setValue('u_active', 'false');
+			oReminder.update();
+			gs.print("Deactivating reminder for " + oReminder.u_approval_for.sysapproval.number);
+		}
+	},
+	
+	cancelApproval : function (sReminder,sApproval) {
+		gs.print("Cancelling SysId " + sReminder + " | " + sApproval);
+		var oReminder = new GlideRecord('u_approval_reminders');
+		if (this.bCancel == 'true') {
+			var oApproval = new GlideRecord('sysapproval_approver');
+			if(oApproval.get(sApproval)) {
+				gs.print("Cancelling " + oApproval.sysapproval.number);
+				oApproval.setValue('state','rejected');
+				oApproval.update();
+			}
+		}
+		if (oReminder.get(sReminder)) {
+			this.deactivateReminder(sReminder);
+		}
+	},
+	
+	getDurationHours : function (dStartDate) {
+		var oSched = new GlideSchedule(this.oSchedId);
+		var dStart = new GlideDateTime(dStartDate);
+		var oDur = oSched.duration(dStart, new GlideDateTime());
+		return oDur.getNumericValue()/3600000;
+		//var iHours = oDur.getNumericValue()/3600000;
+		//return iHours;
+	},
+	
+	getStartDateTime : function(endDateTime, hoursAgo) {
+		endDateTime = endDateTime || new GlideDateTime();
+		hoursAgo = Math.abs(parseInt(hoursAgo)) || 0;
+		var oDurCalc = new DurationCalculator();
+		var startDateTime = new GlideDateTime(endDateTime);
+		// Start with hoursAgo and subtract from there
+		startDateTime.addSeconds(hoursAgo * -3600);
+		this.addSchedule(oDurCalc);
+		while (oDurCalc.calcScheduleDuration(startDateTime, endDateTime) < hoursAgo * 3600) {
+			// Subtract an hour
+			startDateTime.addSeconds(-3600);
+			oDurCalc.setStartDate(startDateTime);
+		}
+		return startDateTime;
+	},
+	
+	
+	addSchedule : function (oDuration) {
+		// Load the schedule into our duration calculator.
+		var sScheduleName = this.oSchedule;
+		var oSched = new GlideRecord('cmn_schedule');
+		oSched.addQuery('name', sScheduleName);
+		oSched.query();
+		if(!oSched.next()){
+			gs.log('*** Could not find schedule "'+ sScheduleName +'"');
+			return;
+		}
+		// Don't pass the timezone value, we get that from the schedule itself
+		oDuration.setSchedule(oSched.getUniqueValue());
+	},
+	
+	/*
+ 	*
+ 	* Internal Functions; called only within SI
+ 	*
+ 	*/
+	
+	_calcNextReminder : function(dStart) {
+		dStart = new GlideDateTime(dStart) || new GlideDateTime();
+		var oSched = new GlideSchedule(this.oSchedId);
+		var oDuration = new GlideDuration(60 * 60 * 1000 * this.iBusinessHours);
+		return oSched.add(dStart, oDuration);
+	},
+	
+	type: 'FAIApprovalReminders'
+};
+
+=============================
+	
+var scReqItemAR = new FAIApprovalReminders('sc_req_item');
+scReqItemAR.insertReminders();
+scReqItemAR.cancelReminders();
+scReqItemAR.sendReminders();
+scReqItemAR.cancelApprovals();
