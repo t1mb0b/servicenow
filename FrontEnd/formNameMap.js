d175c7dc2e04ff2eb71accdec44d13252cd6ea00
Tim <t1mb0b@users.noreply.github.com>
2017-08-25 19:10:35 -0600

    Update formNameMap.js

--- FrontEnd/formNameMap.js
+++ FrontEnd/formNameMap.js
@@ -10,3 +10,13 @@ var helptext = g_form.getControl(g_form.nameMap[index].prettyName);
 toggleHelp(helptext.id);
 }  
 
+=========================
+   
+var chkBoxes = g_form.nameMap.filter(function(chk) {
+return chk.prettyName.indexOf("sso_") > -1;
+});
+chkBoxes.forEach(function(box) {
+console.log(g_form.getControl(box.prettyName));
+});
+//console.log(chkBoxes);
+
