fad59607f2d5d68c6f8c24f00f1688bfb1eca622
Tim <t1mb0b@users.noreply.github.com>
2017-09-01 16:23:24 -0600

    Create reopenIncident.js
    https://community.servicenow.com/thread/281078

---
+++ BackEnd/reopenIncident.js
@@ -0,0 +1,12 @@
+var incidentSysID = "PUT INCIDENT SYSID IN HERE";  
+  
+var incidentRec = new GlideRecord("incident");  
+incidentRec.addQuery("sys_id", incidentSysID);  
+incidentRec.query();  
+while (incidentRec.next()) {  
+  incidentRec.state = 2;  //2 = In Progress, 6 = Resolved  
+  incidentRec.active = true;  
+  incidentRec.autoSysFields(false);  // Do not update sys_updated_on, sys_updated_by, and sys_mod_count  
+  incidentRec.setWorkflow(false);    // Do not run any other business rules  
+  incidentRec.update();  
+}  
