ec799a3cf6af6989d09ba424c80a60223dfce757
Tim Molloy <timkmolloy@gmail.com>
2017-11-19 07:28:09 -0700

    Update to attachmentcopy br

--- BackEnd/BusinessRules/copyAttachments.js
+++ BackEnd/BusinessRules/copyAttachments.js
@@ -1,3 +1,25 @@
 function onAfter(current, previous) {
    GlideSysAttachment.copy('sc_task', current.sysapproval.sys_id, 'sysapproval_approver', current.sys_id);
 }
+
+var attachment = new GlideSysAttachment();
+attachment.deleteAttachment(gr.sys_id);
+
+var gr = new GlideRecord('sys_attachment');
+gr.addQuery('u_record', '4cf6c9e4dbaec3009d2d572e5e9619e0');
+gr.query();
+gr
+  while(gr.next()) {
+    gr.addInfoMessage(gr.file_name);
+  }
+
+  function _checkAttachments() {
+		var attach = new GlideRecord('sysapproval_approver');
+		attach.get(current.sys_id);
+		if(attach.hasAttachments()) {
+			gs.info("TM======>Has attachments");
+			return true;
+		}
+		gs.info("TM======>No attachments");
+		return false;
+	}
