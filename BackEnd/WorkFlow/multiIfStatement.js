f2992a893c0ed0ef6987eaf87730a3262f87cfc1
Tim <t1mb0b@users.noreply.github.com>
2017-11-30 21:55:42 -0700

    Create multiIfStatement.js

---
+++ BackEnd/WorkFlow/multiIfStatement.js
@@ -0,0 +1,33 @@
+workflow.scratchpad.result = ifScript();
+
+function ifScript() {
+	var result = {desk:false, mobile:false, byod:false, activation:false};
+		
+		if (workflow.scratchpad.desk == 'true') {
+			result.desk = true;
+		}
+		if (workflow.scratchpad.mobile == 'true') {
+			result.mobile = true;
+		}
+		if (workflow.scratchpad.byod == 'true') {
+			result.byod = true;
+		}
+		if (workflow.scratchpad.activation == 'true') {
+			result.byod = true;
+		}
+		if (!(result.desk || result.mobile || result.byod || result.activation)) {
+			result.none = true;
+		}
+		
+		//gs.log('---> ' + result.desk + ',' + result.mobile + ',' + result.byod,
+		  // 'Playing With the If Activity.Validate Trigger');
+		return result;
+	}
+  
+  
+  =====
+  
+  Branch:
+  
+  workflow.scratchpad.result.x == true
+  Check 'skip during generate'
