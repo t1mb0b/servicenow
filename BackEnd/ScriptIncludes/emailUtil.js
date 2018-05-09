bf00321fd230cd471fb47a1538e9b61f88b9d256
Tim <t1mb0b@users.noreply.github.com>
2018-05-09 15:51:31 -0600

    Update emailUtil.js

--- BackEnd/ScriptIncludes/emailUtil.js
+++ BackEnd/ScriptIncludes/emailUtil.js
@@ -14,12 +14,30 @@ EmailUtil.prototype = {
 		return text;
 	},
 	
+	removeVarText: function(sText) {
+		if(this._checkForVars(sText)){
+			gs.log("TM===> Stripping Delimeter", "Q: Geu SI");
+			sText = sText.replace(/([\s\S]*?)%BEGIN%[\s\S]*?%END%/g, '$1');
+		}
+		return sText;
+	},
+	
 	_checkForText: function(text) {
 		var loc = text.indexOf("The information contained in this email,");
 		if(loc>0)
 			return true;
 		else
 			return false;
+	},
+	
+	_checkForVars: function(sText) {
+		var sDelimeter = sText.indexOf("%BEGIN%");
+		if(sDelimeter>0) {
+			gs.log("TM===> Found Delimeter", "Q: Geu SI");
+			return true;
+		} else {
+			return false;
+		}
 	},
 	
 	type: 'EmailUtil'
