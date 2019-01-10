5f1d797ea7fc62130b59b7af30216593cc00c5c2
Tim <t1mb0b@users.noreply.github.com>
2019-01-10 14:07:27 -0700

    Create lexicalScopeLoop.js
    https://stackoverflow.com/questions/3755606/what-does-the-exclamation-mark-do-before-the-function

---
+++ BackEnd/lexicalScopeLoop.js
@@ -0,0 +1,15 @@
+var answer = [];
+var appsArr = String(current.variables.pdi_focal_roles).split(",");
+var pdiGr = new GlideRecord("u_pdi_access_roles");
+pdiGr.addQuery('sys_id', 'IN', appsArr);
+pdiGr.query();
+while(pdiGr.next()) {
+	answer.push(pdiGr.getValue('u_module_approver'));
+	if(pdiGr.getValue('u_additional_approvers')) {
+!function pushApprovers() {
+		pdiGr.getValue('u_additional_approvers').split(',').forEach(function(approver) {
+			answer.push(approver);
+		});
+}();
+}
+}
