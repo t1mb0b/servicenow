316be3b4e5d10696d9cdf084ef3eae6b6997d12d
Tim <t1mb0b@users.noreply.github.com>
2019-01-14 13:47:28 -0700

    Update assocArray.js

--- ServicePortal/assocArray.js
+++ ServicePortal/assocArray.js
@@ -54,3 +54,50 @@ while(x in parent)
          </tr>
        </tbody>
      </table>
+
+----
+
+data.divList=[];
+var parent = [];
+var grParent = new GlideRecord('x_usdo2_phone_dire_divisions');
+    grParent.addQuery('active',true);
+    grParent.orderBy('order');
+    grParent.query();
+while(grParent.next())
+{
+    var tempObj = {};
+        tempObj.parent = {};
+        tempObj.parent.id = grParent.sys_id+"";
+        tempObj.parent.room = grParent.getDisplayValue('room_number');
+        tempObj.parent.display = grParent.getDisplayValue('label');
+        tempObj.child = [];
+    parent[grParent.sys_id+""] = tempObj;
+    data.divList.push(tempObj);
+}
+
+for(x in parent)
+{
+    var parentID = parent[x].parent.id+"";
+    
+    var grChild = new GlideRecord('x_usdo2_phone_dire_numbers');
+        grChild.addQuery('division',parentID);
+        grChild.addQuery('active',true);
+        grChild.query();
+    
+    while(grChild.next())
+    {
+        parent[x].child.push({
+                'user_last':grChild.getDisplayValue('last_name'),
+                'user_first':grChild.getDisplayValue('first_name'),
+                'user_office':grChild.getDisplayValue('office'),
+                'user_title':grChild.getDisplayValue('job_title'),
+                'user_div':grChild.getDisplayValue('division'),
+                'user_office_phone':grChild.getDisplayValue('office_phone'),
+                'user_official_cell':grChild.getDisplayValue('official_cell'),
+                'user_home_phone':grChild.getDisplayValue('home_phone'),
+                'user_alternate_phone':grChild.getDisplayValue('alternate_phone'),
+                'user_short_extension':grChild.getDisplayValue('short_extension')
+            });
+        
+    }
+}
