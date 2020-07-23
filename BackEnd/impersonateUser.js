c05fe7061e74c8480168b6416fc7029fa8d82825
Tim <t1mb0b@users.noreply.github.com>
2020-07-23 12:17:45 -0600

    Update impersonateUser.js

--- BackEnd/impersonateUser.js
+++ BackEnd/impersonateUser.js
@@ -20,3 +20,29 @@ function filterRequestItems () {
     //Return the advanced Ref Qualifier  
     return 'sys_idIN' + availableItems;  
 } 
+
+/// BR
+session.onlineImpersonate("abel.tuter");
+var current = GlideRecord('incident');
+if (!gs.hasRole("itil")) {
+  var u = gs.getUserID();
+   var qc = current.addQuery("caller_id", u).addOrCondition("opened_by", u).addOrCondition("watch_list", "CONTAINS", u);
+  gs.print("query restricted to user: " + u);
+}
+ current.query();
+while (current.next()) {
+gs.print(current.number);
+}
+session.onlineUnimpersonate();
+
+//
+var og = gs.getSession().impersonate('5136503cc611227c0183e96598c4f706'); 
+gs.print(gs.getUser().getFullName()); 
+var gr1 = new GlideRecord('sn_customerservice_case'); 
+gr1.addQuery('sys_id',b08cd51fdba83a007a4cbedffe96196d'); 
+gr1.query(); 
+if(gr1.next()) 
+gs.print("Found it"); 
+else 
+gs.print("Not Found"); 
+gs.getSession().impersonate(og); 
