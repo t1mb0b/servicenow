49f92e01d8a26461e3c68b81517c4f62222a1d6a
Tim <t1mb0b@users.noreply.github.com>
2020-10-07 13:26:33 -0600

    Update CascadeRITMCommentsDown.js

--- BackEnd/BusinessRules/CascadeRITMCommentsDown.js
+++ BackEnd/BusinessRules/CascadeRITMCommentsDown.js
@@ -27,3 +27,36 @@
 			oTask.update();
 		}
 })(current, previous);
+
+=====
+	
+//Name: push comments to ritm
+//Table: Catalog Task
+//When: Before
+//Insert: false
+//Author: 'jace'
+//Update:true
+// https://jace.pro/post/2017-09-06-syncing-comments/
+(function executeRule(current, previous /*null when async*/) {
+    try{
+        var sc_req_item = new GlideRecord('sc_req_item');
+        if(sc_req_item.get(current.request_item)){
+            sc_req_item.comments = current.comments;
+            sc_req_item.setUseEngines(false);
+            // Tells the system not to run anything
+            // with "engine" in the name from this list.
+            // https://is.gd/gsNiQZ
+            sc_req_item.update();
+            var email = {};
+            email.sys_id = sc_req_item.u_requested_for.sys_id;
+            email.name = sc_req_item.u_requested_for.getDisplayValue();
+            gs.eventQueue("custom.catalog.ritm.commented",
+                          sc_req_item,
+                          email.sys_id,
+                          email.name);
+        }
+    } catch (error) {
+        var log = 'Push comments to ritm';
+        gs.log('Error: ' + error, log);
+    }
+})(current, previous);
