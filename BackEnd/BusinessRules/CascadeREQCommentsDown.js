0b8dadb86bb6cac8b695366dca0d7c2bcd775adc
Tim <t1mb0b@users.noreply.github.com>
2020-10-07 13:28:12 -0600

    Update CascadeREQCommentsDown.js

--- BackEnd/BusinessRules/CascadeREQCommentsDown.js
+++ BackEnd/BusinessRules/CascadeREQCommentsDown.js
@@ -42,3 +42,41 @@
 		}
 	});
 })(current, previous);
+
+//Name: push comments to tasks
+//Table: Request Item
+//When: Before
+//Insert: false
+//Author: 'jace'
+//Update:true
+(function executeRule(current, previous /*null when async*/) {
+    try{
+        var sc_task = new GlideRecord('sc_task');
+        sc_task.addQuery('request_item', current.sys_id);
+        sc_task.addQuery('active','true');
+        sc_task.query();
+        while(sc_task.next()){
+            sc_task.comments = current.comments;
+            sc_task.setUseEngines(false);
+            // Tells the system not to run anything
+            // with "engine" in the name from this list.
+            // https://is.gd/gsNiQZ
+            sc_task.update();
+            var email = {};
+            if(sc_task.assigned_to){
+                email.sys_id = sc_task.getValue('assigned_to');
+                email.name = sc_task.assigned_to.getDisplayValue();
+            } else {
+                email.sys_id = sc_task.getValue('assignment_group');
+                email.name = sc_task.assignment_group.getDisplayValue();
+            }
+            gs.eventQueue("custom.catalog.ritm.commented",
+                          sc_task,
+                          email.sys_id,
+                          email.name);
+        }
+    } catch (error) {
+        var log = 'Push comments to tasks';
+        gs.log('Error: ' + error, log);
+    }
+})(current, previous);
